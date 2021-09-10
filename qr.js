7ff92c674858d5171e933fac4e7b1ae3fad797f8
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-10 17:54:35 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,43 +1,59 @@
+/* Copyright (C) 2020 Yusuf Usta.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WhatsAsena - Yusuf Usta
+*/
+
 const chalk = require('chalk');
-const {WAConnection} = require('@adiwajshing/baileys');
-const {StringSession} = require('./alexa/');
+const {WAConnection, MessageOptions, MessageType} = require('@adiwajshing/baileys');
+const {StringSession} = require('./whatsasena/');
 const fs = require('fs');
 
-async function Alexa () {
+async function whatsAsena () {
     const conn = new WAConnection();
-    const Session = new StringSession();
-    conn.version = [2, 2119, 6]
+    const Session = new StringSession();  
+    conn.version = [2,2121,7];
     conn.logger.level = 'warn';
     conn.regenerateQRIntervalMs = 50000;
     
     conn.on('connecting', async () => {
-        console.log(`${chalk.green.bold('Whatsdevil')}
-${chalk.white.italic('Made By ')}${chalk.red.bold('TUTUX')}
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+${chalk.white.italic('AsenaString Kodu Alıcı')}
 
-${chalk.blue.italic('🔄 Loading QR CODE...')}`);
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
     });
     
 
-    conn.on('open', () => {
+    conn.on('open', async () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
-            chalk.green.bold('YOUR SESSION: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
         );
         
         if (!fs.existsSync('config.env')) {
-            fs.writeFileSync('config.env', `SESSION="${st}"`);
+            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
         }
-
-        console.log(
-            chalk.red.bold('⚠ COPY THIS CODE, It is an IMPORTANT CODE... ⚠'))
-            
-        console.log(
-            chalk.blue.bold('Thank You For Using Whatsdevil💖'))
-
+        if (conn.user.jid.startsWith('90')) {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Bu Kodu Kimseyle Paylaşmayın!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
+            );
+        }
+        else {
+            await conn.sendMessage(conn.user.jid,st, MessageType.text)
+            await conn.sendMessage(conn.user.jid,'*Do Not Share This Code With Anyone!*', MessageType.text)
+            console.log(
+                chalk.blue.bold('If you are installing locale, you can start the bot with node bot.js')
+            );
+        }
+        
         process.exit(0);
     });
 
     await conn.connect();
 }
 
-Alexa()
+whatsAsena()
